a3d12bd75528cb1f3f919c9c1da0170e40883e9a
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-21 02:45:07 +0800

    Admin - delete product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,11 +6,12 @@ const {
   newProduct,
   getSingleProduct,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 
 router.route("/products").get(getProducts);
 router.route("/product/:id").get(getSingleProduct);
 router.route("/admin/product/new").post(newProduct);
-router.route("/admin/product/:id").put(updateProduct);
+router.route("/admin/product/:id").put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
