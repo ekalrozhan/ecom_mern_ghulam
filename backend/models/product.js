cb7e9a35a94e528603448c4394a1e69b2811e601
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-21 01:47:22 +0800

    Creating data seeder

--- backend/models/product.js
+++ backend/models/product.js
@@ -46,7 +46,7 @@ const productSchema = new mongoose.Schema({
       values: [
         "Electronics",
         "Cameras",
-        "Laptop",
+        "Laptops",
         "Accessories",
         "Headphones",
         "Food",
