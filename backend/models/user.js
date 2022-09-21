66d48eb8db0d0ae0cbf8cf9ed34ece1655f44a1d
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-21 18:09:48 +0800

    Creating user model

---
+++ backend/models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    maxLength: [30, "Your name cannot exceeds 30 characters"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter valid email address"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please enter your passwoor"],
+    minLength: [6, "Your password be longer than 6 characters"],
+    select: false,
+  },
+
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+
+  role: {
+    type: String,
+    default: "user",
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+module.exports = mongoose.model("User", userSchema);
