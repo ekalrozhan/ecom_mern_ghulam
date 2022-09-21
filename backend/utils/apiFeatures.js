2502256dfd6a543cf35fe026bd109fef6b3602fc
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-21 17:24:50 +0800

    Filter products

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -18,6 +18,21 @@ class APIFeatures {
     this.query = this.query.find({ ...keyword });
     return this;
   }
+
+  filter() {
+    const queryCopy = { ...this.queryStr };
+
+    // removing fields from the query
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+
+    // advanced filter for price, rating, etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 }
 
 module.exports = APIFeatures;
