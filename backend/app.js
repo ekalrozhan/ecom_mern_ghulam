275cb3cfad24149b2b178d2fc91f6da0d5eafe45
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-21 18:43:07 +0800

    Encrypting password while registration

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,13 @@ const errorMiddleware = require("./middlewares/errors");
 app.use(express.json());
 // import all routes
 const products = require("./routes/product");
+const auth = require("./routes/auth");
 
 app.use("/api/v1", products);
+app.use("/api/v1", auth);
 
 // middleware to handle errors
 app.use(errorMiddleware);
 
 module.exports = app;
+ 
---
+++ backend/controllers/authController.js
@@ -0,0 +1,22 @@
+const User = require("../models/user");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+
+// register a user =>  /api/v1/register
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "avatars/leio-mclaren-L2dTmhQzx4Q-unsplash_f3yjhn",
+      url: "https://res.cloudinary.com/dmtuoxiiu/image/upload/v1663755796/avatars/leio-mclaren-L2dTmhQzx4Q-unsplash_f3yjhn.jpg",
+    },
+  });
+
+  res.status(201).json({
+    success: true,
+    user,
+  });
+});
