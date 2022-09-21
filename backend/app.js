59b98983c45dadd453a0c5c92fee1da7d5300483
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-21 23:33:03 +0800

    Protected routes from unauthorized user

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middlewares/errors");
 
-app.use(express.json());
 // import all routes
 const products = require("./routes/product");
 const auth = require("./routes/auth");
+
+
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1", products);
 app.use("/api/v1", auth);
@@ -14,4 +19,3 @@ app.use("/api/v1", auth);
 app.use(errorMiddleware);
 
 module.exports = app;
- 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
 
+
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
