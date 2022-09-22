7b35b8b0f23733f1401fa670ca63035de88eaf82
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-22 23:05:03 +0800

    Create and save order

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,14 @@ const errorMiddleware = require("./middlewares/errors");
 // import all routes
 const products = require("./routes/product");
 const auth = require("./routes/auth");
-
-
+const order = require("./routes/order");
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1", products);
 app.use("/api/v1", auth);
+app.use("/api/v1", order);
 
 // middleware to handle errors
 app.use(errorMiddleware);
